c2bb51ebbea1c85bad5fe766120bc1674cf8941c
Angel Byun <abyun22@turing.edu>
2023-01-05 13:39:22 -0700

    Add questions 1 and 2

--- final_practice.rb
+++ final_practice.rb
@@ -1,20 +1,33 @@
 # Defining Our Own Methods
 # 1: Write a method named greeting that prints out a general greeting to someone
 
+greeting = "Hey, I hope you have a great day!"
+puts greeting
+
 # What is the return value of your method?
+# The return value of my method is "Hey, I hope you have a great day!"
+
 # How many arguments did you pass your method?
-
-
+# I did not pass any arguments in my method.
 
 
 #2: Write a method named custom_greeting that prints out a greeting WITH a specific name.
 
+def greeting(name)
+    "Hey, #{name}! I hope you have a great day!"
+end
+
+puts greeting("name")
 
 # What is the return value of your method?
+# The return value of my method is "Hey, ("name")! I hope you have a great day!"
+# In this instance, I put my name "Angel" in the string and the console printed "Hey, Angel! I hope you have a great day!"
+
 # How many arguments did you pass your method?
+# I had one argument pass my method. 
+
 # What data type was your argument(s)?
-
-
+# My data type was the string argument.
 
 
 #3: Write a method named square that takes in one number, and returns the square of that number
